712889aa2a29a2ac560d956a70149b57c557c0a8
Orhan Obut <orhan.obut@rocket-internet.de>
2014-12-24 16:51:17 +0100

    unit test added

---
+++ app/src/androidTest/java/com/orhanobut/waspsample/MyServiceTest.java
@@ -0,0 +1,99 @@
+package com.orhanobut.waspsample;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+
+import com.github.nr4bt.wasp.CallBack;
+import com.github.nr4bt.wasp.Wasp;
+import com.github.nr4bt.wasp.http.GET;
+import com.github.nr4bt.wasp.http.Path;
+
+import junit.framework.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Orhan Obut
+ */
+public class MyServiceTest extends InstrumentationTestCase {
+
+    private static final String TAG = MyServiceTest.class.getSimpleName();
+    
+    Context context;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        System.setProperty(
+                "dexmaker.dexcache",
+                getInstrumentation().getTargetContext().getCacheDir().getPath());
+
+        context = getInstrumentation().getContext();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        context = null;
+    }
+
+    public interface Service1 {
+        @GET("/repos/{user}/{repo}")
+        void fetchRepo(@Path("user") String user,
+                       @Path("repo") String repo
+        );
+    }
+
+    public void test_methodShouldHaveCallBackParam() {
+        try {
+            new Wasp.Builder(context)
+                    .setEndpoint("endpoint")
+                    .build()
+                    .create(Service1.class);
+            Assert.fail("Last param should be callback");
+        } catch (Exception e) {
+            assertThat(e.getMessage());
+        }
+    }
+    
+    public interface Service2 {
+        @GET("/repos/{user}/{repo}")
+        void fetchRepo(@Path("user") String user,
+                       @Path("user") String repo,
+                       CallBack callBack
+        );
+    }
+
+    public void test_pathValueShouldNotBeDuplicated() {
+        try {
+            new Wasp.Builder(context)
+                    .setEndpoint("endpoint")
+                    .build()
+                    .create(Service2.class);
+            Assert.fail("Path value should not be duplicated");
+        } catch (Exception e) {
+            assertThat(e.getMessage());
+        }
+    }
+
+    public interface Service3 {
+        @GET("/repos/{user}/{repo}")
+        void fetchRepo(@Path("user") String user,
+                       CallBack callBack,
+                       @Path("repo") String repo
+        );
+    }
+
+    public void test_callBackShouldBeLastParam() {
+        try {
+            new Wasp.Builder(context)
+                    .setEndpoint("endpoint")
+                    .build()
+                    .create(Service3.class);
+            Assert.fail("Callback should be last param");
+        } catch (Exception e) {
+            assertThat(e.getMessage());
+        }
+    }
+}
